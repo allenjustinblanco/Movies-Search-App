e8a9b85b1bf42977eee4fdfeb352baa89ec20b23
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-02-25 22:33:42 -0600

    feat: view movie

--- src/MovieRow.js
+++ src/MovieRow.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class MovieRow extends Component{
+    viewMovie(){
+        const url = "https://www.themoviedb.org/movie/" + this.props.movie.id;
+        window.location.href = url
+    }
+
     render() {
         return(
             <div>
@@ -11,8 +16,9 @@ class MovieRow extends Component{
                             <img src={this.props.movie.poster_src} width="200" alt="poster"/>
                         </td>
                         <td>
-                            <h1>{this.props.movie.title}</h1>
+                            <h2>{this.props.movie.title}</h2>
                             <p>{this.props.movie.overview}</p>
+                            <input type="button" value="View" onClick={this.viewMovie.bind(this)}/>
                         </td>
                     </tr>
                     </tbody>
